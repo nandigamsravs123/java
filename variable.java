cb82e7439c251fb17668865ce2bc977b31218db1
nandigamsravs123 <sravsnandigam@gmail.com>
2025-07-20 21:27:01 +0530

    Update variable.java

--- variable.java
+++ variable.java
@@ -1,6 +1,6 @@
 public class variable {
     public static  void main(String[]args){
-        int a =10;
+        int a =10;//varialble is container to store a value with datatype
         
         System.out.println(a);
 }
