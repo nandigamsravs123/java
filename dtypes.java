b23bf2237be5a8b6e46407f97748927831b414ae
nandigamsravs123 <sravsnandigam@gmail.com>
2025-07-20 21:25:03 +0530

    data-types

---
+++ dtypes.java
@@ -0,0 +1,23 @@
+public class dtypes {
+    public static  void main(String[]args){
+        int a =10;//declare int datatype - one byte->default=>0 -128 to +128
+        short b =20;// -32768 to +23768
+        byte c= -30;//-2147482648 to  +2147482648
+        long d= 100;//-9237203685 to +9237203685
+        float e = 0.07f; //decimal digits-4 bytes
+        char f = 's';//single quotes and single letter
+        String g = "this is shravya";//doule qoutes-text
+        
+        System.out.println("a:"+a);
+        System.out.println("b:"+b);
+        System.out.println("c:"+c);
+        System.out.println("d:"+d);
+        System.out.println("e:"+e);
+        System.out.println("f:"+f);
+        System.out.println("g:"+g);
+      
+
+}
+}
+    
+
