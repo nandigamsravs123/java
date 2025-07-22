93344e103fd170ee424512383d22d7cb5d65be06
nandigamsravs123 <sravsnandigam@gmail.com>
2025-07-23 00:13:22 +0530

    abstraction

--- oops/abstraction.java
+++ oops/abstraction.java
@@ -1,5 +1,25 @@
-package oops;
+
+abstract class Animal {
+    // Abstract method (no body)
+    abstract void makeSound();
+
+    // Regular method
+    void sleep() {
+        System.out.println("Sleeping...");
+    }
+}
+
+class Dog extends Animal {
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
 
 public class abstraction {
-    
+    public static void main(String[] args) {
+        Animal a = new Dog(); // Upcasting
+        a.makeSound(); // Output: Dog barks
+        a.sleep();     // Output: Sleeping...
+    }
 }
+
