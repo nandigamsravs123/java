52a31016fe9424e44060250b7d0b66ed3f9717a6
nandigamsravs123 <sravsnandigam@gmail.com>
2025-07-23 21:37:13 +0530

    polymorphism

---
+++ polymorphism.java
@@ -0,0 +1,35 @@
+
+//class Adder {//method overloading
+   // static int add(int a, int b) {
+      //  return a + b;
+   // }
+
+   // static double add(double a, double b) {
+     //   return a + b;
+    //}
+//}
+
+//overiding
+
+
+class Animal {
+    public void displayInfo() {
+        System.out.println("I am an animal");
+    }
+}
+
+class Dog extends Animal {
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("I bark");
+    }
+}
+
+public class polymorphism {
+    public static void main(String[] args) {
+        Animal d1 = new Dog();  // Polymorphism: parent reference, child object
+        d1.displayInfo();       // Output: I bark
+    }
+}
+
+//method overiding
